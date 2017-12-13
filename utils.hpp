a1f4a20628bd16976cf323ed7a6c21ae3e033f9a
Martino Facchin <m.facchin@arduino.cc>
2017-12-13 22:59:46 +0100

    [WIN] escape all string, not only the first occurrency
    Fixes https://github.com/arduino/arduino-preprocessor/issues/5

--- utils.hpp
+++ utils.hpp
@@ -31,6 +31,7 @@
 
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
@@ -77,16 +78,18 @@ inline bool cStrEndsWith(const char *str, const char *suffix) {
 }
 
 #ifdef WIN32
-inline bool replace(std::string& str, const std::string& from, const std::string& to) {
-    size_t start_pos = str.find(from);
-    if(start_pos == std::string::npos)
-        return false;
-    str.replace(start_pos, from.length(), to);
-    return true;
+
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
+    return str;
 }
 
 inline std::string quoteCppString(std::string& str) {
-    replace(str, "\\", "\\\\");
+    str = ReplaceAll(str, "\\", "\\\\");
     return str;
 }
 #else
